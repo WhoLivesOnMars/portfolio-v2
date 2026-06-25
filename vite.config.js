55ebadb79d6d0cbc62d25d7bca4ddc90e8b7d239
Daria KHANINA <khaninadaria@gmail.com>
2026-06-25 19:49:38 +0200

    fix: configure vite base for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isVercel = process.env.VERCEL === "1";
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio-v2/',
+  base: isVercel ? "/" : "/portfolio-v2/",
   plugins: [react()],
 })
